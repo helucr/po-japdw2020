49c21c97d444597384f62c4704bc49f7ee18f57f
Helena Cruz <litau07@gmail.com>
2020-08-06 20:56:54 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -2,29 +2,8 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-
-    var getJSONData = function(url) {
-        var result = {};
-        showSpinner();
-        return fetch(PRODUCTS_INFO_URL)
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw Error(response.statusText);
+            getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
+                if (resultObj.status === "ok") {
+                    sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
                 }
-            })
-            .then(function(response) {
-                result.status = 'ok';
-                result.data = response;
-                hideSpinner();
-                return result;
-            })
-            .catch(function(error) {
-                result.status = 'error';
-                result.data = error;
-                hideSpinner();
-                return result;
             });
-    }
-});
