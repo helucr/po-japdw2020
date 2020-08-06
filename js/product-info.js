b00cc4195dcffab4342798dd1b1aa56e0ef95563
Helena Cruz <litau07@gmail.com>
2020-08-06 20:42:02 -0300

    Petición web JSON Entrega1
    Petición web JSON Entrega1

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,30 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 
+    var getJSONData = function(url) {
+        var result = {};
+        showSpinner();
+        return fetch(PRODUCTS_INFO_URL)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw Error(response.statusText);
+                }
+            })
+            .then(function(response) {
+                result.status = 'ok';
+                result.data = response;
+                hideSpinner();
+                return result;
+            })
+            .catch(function(error) {
+                result.status = 'error';
+                result.data = error;
+                hideSpinner();
+                return result;
+            });
+    }
 });
