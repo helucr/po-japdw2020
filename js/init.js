77935af4f01199e4c76aeae99d896235ada582ee
Helena Cruz <litau07@gmail.com>
2020-08-06 20:39:32 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -18,7 +18,7 @@ var hideSpinner = function() {
 var getJSONData = function(url) {
     var result = {};
     showSpinner();
-    return fetch(PRODUCTS_INFO_URL)
+    return fetch(PRODUCTS_URL)
         .then(response => {
             if (response.ok) {
                 return response.json();
