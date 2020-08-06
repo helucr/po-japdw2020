77e17dac8ff5c12dc418b3f3eaa77766a5a3a1a6
Helena Cruz <litau07@gmail.com>
2020-08-06 20:50:01 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,31 +3,31 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
 
+        var getJSONData = function(url) {
+            var result = {};
+            showSpinner();
+            return fetch(PRODUCTS_URL)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw Error(response.statusText);
+                    }
+                })
+                .then(function(response) {
+                    result.status = 'ok';
+                    result.data = response;
+                    hideSpinner();
+                    return result;
+                })
+                .catch(function(error) {
+                    result.status = 'error';
+                    result.data = error;
+                    hideSpinner();
+                    return result;
+                });
 
-
-});
-
-var getJSONData = function(url) {
-    var result = {};
-    showSpinner();
-    return fetch(PRODUCTS_URL)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw Error(response.statusText);
-            }
-        })
-        .then(function(response) {
-            result.status = 'ok';
-            result.data = response;
-            hideSpinner();
-            return result;
-        })
-        .catch(function(error) {
-            result.status = 'error';
-            result.data = error;
-            hideSpinner();
-            return result;
         });
+
+
 }
