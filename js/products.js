8cdb9403c2873db584b8362f49422f086fb6bb00
Helena Cruz <litau07@gmail.com>
2020-08-08 17:00:28 -0300

    reinicio entrega 1

--- js/products.js
+++ js/products.js
@@ -1,13 +1,6 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-
-document.addEventListener("DOMContentLoaded", function(e) {
-    getJSONData(PRODUCTS_URL).then(data => {
-        console.log(data)
-        var contenido = data;
-        document.getElementById("prodContainer").innerHTML = ''
-
-    })
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
