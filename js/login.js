e00ebd43fa64941705494037152ff1de0d44879a
Helena Cruz <litau07@gmail.com>
2020-08-14 10:04:36 -0300

    Validación JS formulario login y iniciar sitio en login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,25 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+
+
+function verifyLogin() {
+    let userid = document.getElementById("userid");
+    let userpass = document.getElementById("userpass");
+
+    if (userid.value === null || userid.value === "") {
+        console.log = userid.value
+        userid.setCustomValidity("Ingresa tu nombre de usuario")
+    }
+    if (userpass.value.length === 0) {
+        console.log = userpass.value.length
+        userpass.setCustomValidity("Ingresa tu contraseña")
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function(e) {
+
 
 });
