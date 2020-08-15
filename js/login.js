f9e1f11535a287c81e1d7fea7cf2e1a623c1c4c6
Helena Cruz <litau07@gmail.com>
2020-08-15 20:14:49 -0300

    Entrega 1
    1- Petición web a url en formato JSON (se trabaja sobre products.js y products.html)
    2- Se crea página de login en login.html, la validación ficticia del formulario se realiza desde login.js
    3- notlogged.js se referencia en todas las paginas html excepto en login.html, esto verifica si el usuario está logueado y en caso negativo lo redirige automáticamente a login.html
    (falta encontrar manera de borrar localstorage para que no quede logueado permanentemente)
    * Falta etapa desafiante de logueo con Google *

--- js/login.js
+++ js/login.js
@@ -1,27 +1,36 @@
+function verifyLogin() {
+
+    /* Declaro variables que contendran el valor de los input del loginform */
+    let useridvalue = document.getElementById('userid').value;
+    let userpassvalue = document.getElementById('userpass').value;
+
+    /* Selecciono inputs */
+    let userinput = document.getElementById('userid');
+    let passinput = document.getElementById('userpass');
+
+    /* Verificar campos requeridos en loginform */
+    if (useridvalue === null || useridvalue === '') {
+        console.log = useridvalue;
+        userinput.setCustomValidity('Ingresa tu nombre de usuario');
+
+    }
+    if (userpassvalue.length < 8) {
+        console.log = userpassvalue.length;
+        passinput.setCustomValidity('La contraseña debe tener al menos 8 caracteres.');
+
+    } else {
+        /* guardar en localStorage  */
+        localStorage.setItem('user', useridvalue);
+        console.log = localStorage.getItem('user')
+
+
+    };
+};
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+document.addEventListener('DOMContentLoaded', function(e) {
 
-/* Declaro variables que contendran el valor de los input del loginform */
-
-
-
-/* Verificar campos requeridos en loginform */
-
-let userid = document.getElementById("userid");
-let userpass = document.getElementById("userpass");
-
-function verifyLogin() {
-    if (userid.value === null || userid.value === "") {
-        console.log = userid.value
-        userid.setCustomValidity("Ingresa tu nombre de usuario")
-    }
-    if (userpass.value.length < 8) {
-        console.log = userpass.value.length
-        userpass.setCustomValidity("La contraseña debe tener al menos 8 caracteres.")
-    } else {
-        /* cambiar la variable logged a true y guardar en localstorage  */
-        localStorage.setItem("user", "userid.value")
-        localStorage.setItem("pass", "userpass.value")
-    };
-};
+});
