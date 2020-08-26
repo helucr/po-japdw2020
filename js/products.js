bacd3e3e78c7fce05cf083477b3f321f0a8bab3b
Helena Cruz <litau07@gmail.com>
2020-08-25 21:02:32 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -26,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
                         <div class="col">
                             <div class="d-flex w-100 justify-content-between">
                                 <h4 class="mb-1">` + product.name + `</h4>
+                                <small class="text-muted">` + product.soldCount + ` vendidos.</small>
                             </div>
                             <p class="mb-1">` + product.description + `</p>
                             <br>
