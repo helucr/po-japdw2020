5dd0d69f876d4fbe915275d56f68c57397bfd5fd
Helena Cruz <litau07@gmail.com>
2020-08-28 00:19:26 -0300

    Entrega 2
    Mostrar listado de productos en productos.html filtrando por precio y relevancia ascendente y descendente.

--- js/products.js
+++ js/products.js
@@ -1,43 +1,123 @@
+const ORDER_ASC_BY_COST = "AZ";
+const ORDER_DESC_BY_COST = "ZA";
+const ORDER_BY_SOLD_COUNT_ASC = "CantUp.";
+const ORDER_BY_SOLD_COUNT_DESC = "Cant.";
+var currentProductsArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
+
+function sortProducts(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_COST) {
+        result = array.sort(function(a, b) {
+            if (a.cost < b.cost) { return -1; }
+            if (a.cost > b.cost) { return 1; }
+            return 0;
+        });
+
+    } else if (criteria === ORDER_DESC_BY_COST) {
+        result = array.sort(function(a, b) {
+            if (a.cost > b.cost) { return -1; }
+            if (a.cost < b.cost) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_SOLD_COUNT_DESC) {
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount < bCount) { return -1; }
+            if (aCount > bCount) { return 1; }
+            return 0;
+
+        });
+
+    } else if (criteria === ORDER_BY_SOLD_COUNT_ASC) {
+        result = array.sort(function(a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+
+        });
+
+    }
+
+    return result;
+}
+
+function sortAndShowProducts(sortCriteria, productsArray) {
+    currentSortCriteria = sortCriteria;
+
+    if (productsArray != undefined) {
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    showProductsList();
+}
+
+
+function showProductsList() {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentProductsArray.length; i++) {
+        let product = currentProductsArray[i];
+
+        htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">` + product.name + `</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                        <br>
+                        <h5>` + product.cost + ` ` + product.currency + `.</h5>
+                    </div>
+                </div>
+            </a>
+            `
+    }
+    document.getElementById("prodList").innerHTML = htmlContentToAppend;
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-
-    showSpinner()
-
-    function getJSON() {
-        fetch(PRODUCTS_URL)
-            .then(response => response.json())
-            .then(data => {
-                productList(data)
-            })
-    }
-    getJSON()
-
-    function productList(data) {
-        let content = document.getElementById("prodList")
-        content.innerHTML = ""
-        for (let product of data) {
-            content.innerHTML += `
-                    <div class="row">
-                        <div class="col-3">
-                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                        </div>
-                        <div class="col">
-                            <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">` + product.name + `</h4>
-                                <small class="text-muted">` + product.soldCount + ` vendidos.</small>
-                            </div>
-                            <p class="mb-1">` + product.description + `</p>
-                            <br>
-                            <h5>` + product.cost + ` ` + product.currency + `.</h5>
-                        </div>
-                    </div>
-                    <hr>
-                `
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowProducts(ORDER_ASC_BY_COST, resultObj.data);
         }
-        hideSpinner()
-    }
+    });
+
+    document.getElementById("sortAsc").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_ASC_BY_COST);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_DESC_BY_COST);
+    });
+
+    document.getElementById("sortByCountAsc").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_BY_SOLD_COUNT_ASC);
+    });
+
+    document.getElementById("sortByCountDesc").addEventListener("click", function() {
+        sortAndShowProducts(ORDER_BY_SOLD_COUNT_DESC);
+    });
+
+
+
 
 
 });
