08e1644ae27f8b1f09175647d107846381c88ff1
Helena Cruz <litau07@gmail.com>
2020-09-13 21:02:23 -0300

    Cerrar sesión

--- js/init.js
+++ js/init.js
@@ -40,12 +40,24 @@ var getJSONData = function(url) {
         });
 }
 
-
+function logOut() {
+    localStorage.removeItem('user');
+}
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener('DOMContentLoaded', function(e) {
-    document.getElementsByClassName("container d-flex flex-column flex-md-row justify-content-between")[0].innerHTML += `<a class="py-2 d-none d-md-inline-block" href=""> ${localStorage.getItem('user')} </a>`
+    document.getElementsByClassName("container d-flex flex-column flex-md-row justify-content-between")[0].innerHTML +=
+        `<div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                ${localStorage.getItem('user')}
+             </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="my-profile.html">Mi perfil</a>
+            <a class="dropdown-item" href="cart.html">Mi carrito</a>
+            <a class="dropdown-item" href="cart.html" id='LogOut' onclick=logOut()>Cerrar Sesión</a>
+        </div>
+    </div>`
 });
