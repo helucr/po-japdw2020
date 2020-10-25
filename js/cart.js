d0d12bfca38efd9740ada63b82b40a1781811288
Helena Cruz <litau07@gmail.com>
2020-10-24 22:32:50 -0300

    Entrega 6 - Modal pago + calcuar envio + validacion compra final y limpiar carrito

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,8 @@ let arrayArticles = [];
 let allSubtotals = 0;
 let rateUSD = 40;
 let shippingCost = 0;
+let confirmedAddress = false
+let confirmedPaymentMethod = false
 
 /* esta funcion recibe un producto y devuelve el costo total segun su precio unitario y la cantidad a comprar, si el valor está en pesos utilizando rateUSD se convierte el precio a Dólares */
 function subtotalPrice(count, index) {
@@ -75,10 +77,110 @@ function calcShippingCost() {
 
 }
 
+// MODAL DIRECCION DE ENVIO
+/* BOTON GUARDAR ---> verificar direccion de entrega */
+function verifyShippingAddress() {
+
+    if (document.getElementById("shippingAddress").value === "") {
+        alert("Ingrese la calle");
+    } else if (document.getElementById("shippingAddressNum").value === "") {
+        alert("Ingrese el número de puerta")
+    } else if (document.getElementById("shippingCountry").value === "") {
+        alert("Seleccione el país de destino")
+    } else {
+        confirmedAddress = true;
+        $('#modalShippingAdress').modal('toggle');
+        document.getElementById('btnSelectAddress').innerHTML = "Modificar dirección de entrega.";
+
+    }
+
+};
+
+/* BOTON CANCELAR ---> cierra y limpia los campos */
+function clearShippingAddress() {
+    document.getElementById("shippingAddress").value = "";
+    document.getElementById("shippingAddressNum").value = "";
+    document.getElementById("shippingAddressNumApto").value = "";
+    document.getElementById("shippingCountry").value = ""
+    confirmedAddress = false;
+    document.getElementById('btnSelectAddress').innerHTML = "Ingrese dirección de entrega."
+}
+
+// MODAL FORMA DE PAGO
+/* Deshabilitar campos transferencia bancaria */
+function disableBankPayment() {
+    document.getElementById("cardNumber").disabled = false;
+    document.getElementById("inputcardExpMonth").disabled = false;
+    document.getElementById("inputcardExpYear").disabled = false;
+    document.getElementById("cardcvv").disabled = false;
+
+    document.getElementById("accountNumber").disabled = true;
+    document.getElementById("accountNumber").value = "";
+};
+
+/* Deshabilitar campos pago con tarjeta */
+function disableCardPayment() {
+    document.getElementById("cardNumber").disabled = true;
+    document.getElementById("cardNumber").value = "";
+    document.getElementById("inputcardExpMonth").disabled = true;
+    document.getElementById("inputcardExpMonth").value = "";
+    document.getElementById("inputcardExpYear").disabled = true;
+    document.getElementById("inputcardExpYear").value = "";
+    document.getElementById("cardcvv").disabled = true;
+    document.getElementById("cardcvv").value = "";
+
+    document.getElementById("accountNumber").disabled = false;
+};
+
+/* verificar método de pago */
+function verifyPaymentMethod() {
+    confirmedPaymentMethod = true;
+    $('#modalPaymentMethod').modal('toggle');
+    document.getElementById('btnPaymentOptions').innerHTML = "Modificar forma de pago.";
+
+
+};
+
+function clearPaymentMethod() {
+    document.getElementById("cardNumber").value = "";
+    document.getElementById("inputcardExpMonth").value = "";
+    document.getElementById("inputcardExpYear").value = "";
+    document.getElementById("cardcvv").value = "";
+    document.getElementById("accountNumber").value = ""
+    confirmedPaymentMethod = false;
+    document.getElementById('btnPaymentOptions').innerHTML = "Seleccionar forma de pago.";
+}
+
+/* BTN FINALIZAR COMPRA */
+function buyProducts() {
+    if (shippingCost !== 0 && confirmedAddress === true &&
+        confirmedPaymentMethod === true) {
+        alert('HA FINALIZDO SU COMPRA CON ÉXITO')
+        clearPaymentMethod();
+        clearShippingAddress();
+        document.getElementById('cart-products').innerHTML = "";
+        document.getElementById('subtotalCost').innerHTML = " - ";
+        document.getElementById('shippingCost').innerHTML = " - ";
+        document.getElementById('totalCost').innerHTML = " - ";
 
 
 
 
+
+    } else if (confirmedAddress === false) {
+        alert('Debe seleccionar el tipo de envío.')
+    } else if (shippingCost === 0) {
+        alert('Debe ingresar la dirección de entrega.')
+    } else if (confirmedPaymentMethod === false) {
+        alert('Debe ingresar un método de pago.')
+    }
+
+
+
+
+
+
+};
 
 
 
@@ -113,12 +215,6 @@ function showCartArticles(articles) {
 
 }
 
-/* VALIDACIÓN FORMA DE PAGO */
-
-function payMethodValid() {
-
-}
-
 
 
 
@@ -133,6 +229,16 @@ document.addEventListener("DOMContentLoaded", function(e) {
         if (resultObj.status === 'ok') {
             arrayArticles = resultObj.data.articles;
             showCartArticles(arrayArticles);
+
+
         }
+
     });
+
+
+
+
+
+
+
 })
